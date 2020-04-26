406f8632da9e757b30e1345ce8718d2d21266014
Isabel Rubim <isabel@bluetechno.com.br>
2020-04-26 15:55:52 -0300

    deploy api fake

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://my-json-server.typicode.com/isabelrubim/cutekeyboard-store',
 });
 
 export default api;
